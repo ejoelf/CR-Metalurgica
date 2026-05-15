7c8c1cb75cc8fc5868177e486ce19f0da09088ab
Joel Fernandez <108027704+ejoelf@users.noreply.github.com>
2026-05-15 11:37:23 +0200

    Web: cargar estilos Neo Industrial Blueprint

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './styles/global.css';
 import './styles/fixes.css';
+import './styles/neo-industrial.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
